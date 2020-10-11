5aa489f6dfcacc441d9410d6358ef982b9368e62
SenseiHaketon <sinisahamburg97@gmail.com>
2020-10-11 22:49:15 +0200

    Add assert to check if we are using the correct OpenGL Version (4.5 or higher) Fix orthographic camera movement to move relative to its rotation

--- Haketon/src/Platform/OpenGL/OpenGLContext.cpp
+++ Haketon/src/Platform/OpenGL/OpenGLContext.cpp
@@ -22,6 +22,15 @@ namespace Haketon {
 		HK_CORE_INFO("   Vendor: {0}", glGetString(GL_VENDOR));
 		HK_CORE_INFO("   Renderer: {0}", glGetString(GL_RENDERER));
 		HK_CORE_INFO("   Version: {0}", glGetString(GL_VERSION));
+
+	#ifdef HK_ENABLE_ASSERTS
+		int versionMajor;
+		int versionMinor;
+		glGetIntegerv(GL_MAJOR_VERSION, &versionMajor);
+		glGetIntegerv(GL_MINOR_VERSION, &versionMinor);
+
+		HK_CORE_ASSERT(versionMajor > 4 || (versionMajor == 4 && versionMinor >= 5), "Haketon requires OpenGL version 4.5 or higher!");
+	#endif
 	}
 
 	void OpenGLContext::SwapBuffers()
