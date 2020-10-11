101655c0a2623cf9cbf03f2c8186403d0d855c06
SenseiHaketon <sinisahamburg97@gmail.com>
2020-10-12 00:05:21 +0200

    Add OpenGL message log

--- Haketon/src/Platform/Windows/WindowsWindow.cpp
+++ Haketon/src/Platform/Windows/WindowsWindow.cpp
@@ -4,6 +4,7 @@
 #include "Haketon/Events/ApplicationEvent.h"
 #include "Haketon/Events/MouseEvent.h"
 #include "Haketon/Events/KeyEvent.h"
+#include "Haketon/Renderer/Renderer.h"
 
 #include "Platform/OpenGL/OpenGLContext.h"
 
@@ -50,7 +51,11 @@ namespace Haketon
 			s_GLFWInitialized = true;
 		}
 
-		
+	#if defined(HK_DEBUG)
+		if(Renderer::GetAPI() == RendererAPI::API::OpenGL)
+			glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, GLFW_TRUE);
+	#endif
+
 
 		m_Window = glfwCreateWindow((int)props.Width, (int)props.Height, m_Data.Title.c_str(), nullptr, nullptr);
 
