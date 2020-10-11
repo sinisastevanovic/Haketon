a19978cb69af6df448c090b42555350b5e05f804
SenseiHaketon <sinisahamburg97@gmail.com>
2020-10-12 01:24:59 +0200

    Revert orthographic camera fix, as it creates unwanted behaviour

--- Haketon/src/Haketon/Renderer/OrthographicCamera.cpp
+++ Haketon/src/Haketon/Renderer/OrthographicCamera.cpp
@@ -20,7 +20,7 @@ namespace Haketon
     void OrthographicCamera::RecalculateViewMatrix()
     {
         // Calculate the transform matrix
-        glm::mat4 transform = glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0.0f, 0.0f, 1.0f)) * glm::translate(glm::mat4(1.0f), m_Position);
+        glm::mat4 transform = glm::translate(glm::mat4(1.0f), m_Position) * glm::rotate(glm::mat4(1.0f), glm::radians(m_Rotation), glm::vec3(0.0f, 0.0f, 1.0f));
 
         m_ViewMatrix = glm::inverse(transform);
         m_ViewProjectionMatrix = m_ProjectionMatrix * m_ViewMatrix;
