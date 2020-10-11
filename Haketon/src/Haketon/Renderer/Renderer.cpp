e6e873ca0a694e924507e3003e3b857c595e9a17
SenseiHaketon <sinisahamburg97@gmail.com>
2020-10-11 23:21:22 +0200

    Rename m_SceneData to s_SceneData for naming convenction

--- Haketon/src/Haketon/Renderer/Renderer.cpp
+++ Haketon/src/Haketon/Renderer/Renderer.cpp
@@ -3,7 +3,7 @@
 
 namespace Haketon {
 
-	Renderer::SceneData* Renderer::m_SceneData = new Renderer::SceneData;
+	Renderer::SceneData* Renderer::s_SceneData = new Renderer::SceneData;
 
 	void Renderer::Init()
 	{
@@ -17,7 +17,7 @@ namespace Haketon {
 
 	void Renderer::BeginScene(OrthographicCamera& camera)
 	{
-		m_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
+		s_SceneData->ViewProjectionMatrix = camera.GetViewProjectionMatrix();
 	}
 
 	void Renderer::EndScene()
@@ -27,7 +27,7 @@ namespace Haketon {
 	void Renderer::Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertexArray, const glm::mat4& transform)
 	{
 		shader->Bind();
-		shader->SetMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		shader->SetMat4("u_ViewProjection", s_SceneData->ViewProjectionMatrix);
 		shader->SetMat4("u_Transform", transform);
 		
 		vertexArray->Bind();
--- Haketon/src/Haketon/Renderer/Renderer.h
+++ Haketon/src/Haketon/Renderer/Renderer.h
@@ -25,7 +25,7 @@ namespace Haketon {
 			glm::mat4 ViewProjectionMatrix;
 		};
 
-		static SceneData* m_SceneData;
+		static SceneData* s_SceneData;
 	};
 
 }
