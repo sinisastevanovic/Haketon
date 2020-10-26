df371ee1ee07039db91ef94f2a1afc9307ebb3e9
SenseiHaketon <sinisahamburg97@gmail.com>
2020-10-26 21:22:12 +0100

    Fix ShaderDataType sizes

--- Haketon/src/Haketon/Renderer/Buffer.h
+++ Haketon/src/Haketon/Renderer/Buffer.h
@@ -11,7 +11,7 @@ namespace Haketon {
 	{
 		switch (type)
 		{
-			case ShaderDataType::Float:	return 4;
+			case ShaderDataType::Float:		return 4;
 			case ShaderDataType::Float2:	return 4 * 2;
 			case ShaderDataType::Float3:	return 4 * 3;
 			case ShaderDataType::Float4:	return 4 * 4;
@@ -52,8 +52,8 @@ namespace Haketon {
 				case ShaderDataType::Float2:	return 2;
 				case ShaderDataType::Float3:	return 3;
 				case ShaderDataType::Float4:	return 4;
-				case ShaderDataType::Mat3:		return 3 * 3;
-				case ShaderDataType::Mat4:		return 4 * 4;
+				case ShaderDataType::Mat3:		return 3;
+				case ShaderDataType::Mat4:		return 4;
 				case ShaderDataType::Int:		return 1;
 				case ShaderDataType::Int2:		return 2;
 				case ShaderDataType::Int3:		return 3;
