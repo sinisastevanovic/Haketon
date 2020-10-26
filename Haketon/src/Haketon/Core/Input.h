b4deeabbfd9e0f109f13057c8193caa5cc752527
SenseiHaketon <sinisahamburg97@gmail.com>
2020-10-26 22:02:45 +0100

    Refactored Input

--- Haketon/src/Haketon/Core/Input.h
+++ Haketon/src/Haketon/Core/Input.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#include "Haketon/Core/Core.h"
 #include "KeyCodes.h"
 #include "MouseCodes.h"
 
@@ -10,30 +9,13 @@ namespace Haketon {
 
 	class Input
 	{
-	protected:
-		Input() = default;
+
 	public:
-		virtual ~Input() = default;
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
-
+		static bool IsKeyPressed(KeyCode key);
 		
-		inline static bool IsKeyPressed(KeyCode key) { return s_Instance->IsKeyPressedImpl(key); }		
-		
-		inline static bool IsMouseButtonPressed(MouseCode button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		inline static glm::vec2 GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-		
-		static Scope<Input> Create();
-	protected:
-		virtual bool IsKeyPressedImpl(KeyCode keycode) = 0;
-		virtual bool IsMouseButtonPressedImpl(MouseCode button) = 0;
-		virtual glm::vec2 GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-
-	private:
-		static Scope<Input> s_Instance;
+		static bool IsMouseButtonPressed(MouseCode button);
+		static glm::vec2 GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 }
--- Haketon/src/Platform/Windows/WindowsInput.cpp
+++ Haketon/src/Platform/Windows/WindowsInput.cpp
@@ -1,26 +1,26 @@
 #include "hkpch.h"
-#include "WindowsInput.h"
+#include "Haketon/Core/Input.h"
 
 #include "Haketon/Core/Application.h"
 #include <GLFW/glfw3.h>
 
 namespace Haketon {
 
-	bool WindowsInput::IsKeyPressedImpl(KeyCode key)
+	bool Input::IsKeyPressed(KeyCode key)
 	{
 		auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		auto state = glfwGetKey(window, static_cast<int32_t>(key));
 		return state == GLFW_PRESS || state == GLFW_REPEAT;
 	}
 
-	bool WindowsInput::IsMouseButtonPressedImpl(MouseCode button)
+	bool Input::IsMouseButtonPressed(MouseCode button)
 	{
 		auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		int state = glfwGetMouseButton(window, static_cast<int32_t>(button));
 		return state == GLFW_PRESS;
 	}
 
-	glm::vec2 WindowsInput::GetMousePositionImpl()
+	glm::vec2 Input::GetMousePosition()
 	{
 		auto window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
 		double xPos, yPos;
@@ -29,12 +29,12 @@ namespace Haketon {
 		return { (float)xPos, (float)yPos };
 	}
 
-	float WindowsInput::GetMouseXImpl()
+	float Input::GetMouseX()
 	{
 		return GetMousePosition().x;
 	}
 
-	float WindowsInput::GetMouseYImpl()
+	float Input::GetMouseY()
 	{
 		return GetMousePosition().y;
 	}
