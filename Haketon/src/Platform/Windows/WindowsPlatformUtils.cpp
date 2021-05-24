dd7df9d93b4c171180b0fcf8566546921eabcc6e
SenseiHaketon <sinisahamburg97@gmail.com>
2021-05-24 20:10:53 +0200

    FileDialogs now start at working directory

--- Haketon/src/Platform/Windows/WindowsPlatformUtils.cpp
+++ Haketon/src/Platform/Windows/WindowsPlatformUtils.cpp
@@ -16,11 +16,14 @@ namespace Haketon
     {
         OPENFILENAMEA ofn;          
         CHAR szFile[260] = { 0 };   
+        CHAR currentDir[256] = { 0 };   
         ZeroMemory(&ofn, sizeof(OPENFILENAME));
         ofn.lStructSize = sizeof(OPENFILENAME);
         ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());
         ofn.lpstrFile = szFile;
         ofn.nMaxFile = sizeof(szFile);
+        if(GetCurrentDirectoryA(256, currentDir))
+            ofn.lpstrInitialDir = currentDir;
         ofn.lpstrFilter = Filter;
         ofn.nFilterIndex = 1;
         ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
@@ -35,15 +38,18 @@ namespace Haketon
     std::string FileDialogs::SaveFile(const char* Filter)
     {
         OPENFILENAMEA ofn;          
-        CHAR szFile[260] = { 0 };   
+        CHAR szFile[260] = { 0 };
+        CHAR currentDir[256] = { 0 }; 
         ZeroMemory(&ofn, sizeof(OPENFILENAME));
         ofn.lStructSize = sizeof(OPENFILENAME);
         ofn.hwndOwner = glfwGetWin32Window((GLFWwindow*)Application::Get().GetWindow().GetNativeWindow());
         ofn.lpstrFile = szFile;
         ofn.nMaxFile = sizeof(szFile);
+        if(GetCurrentDirectoryA(256, currentDir))
+            ofn.lpstrInitialDir = currentDir;
         ofn.lpstrFilter = Filter;
         ofn.nFilterIndex = 1;
-        ofn.Flags = OFN_PATHMUSTEXIST | OFN_FILEMUSTEXIST | OFN_NOCHANGEDIR;
+        ofn.Flags = OFN_PATHMUSTEXIST | OFN_OVERWRITEPROMPT | OFN_NOCHANGEDIR;
         if(GetSaveFileNameA(&ofn) == TRUE)
         {
             return ofn.lpstrFile;
