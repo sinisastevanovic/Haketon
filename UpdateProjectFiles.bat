2331c3314b9231719d01fe7c1db3232b0b27f89d
Sinisa Stevanovic <sinisahamburg97@gmail.com>
2023-01-04 20:06:41 +0100

    Update UpdateProjectFiles.bat to check if all premake5 scripts for ThirdParty libs exist

--- UpdateProjectFiles.bat
+++ UpdateProjectFiles.bat
@@ -1,5 +1,7 @@
 @echo off
 pushd ..
+if not exist Haketon\ThirdParty\GLFW\premake5.lua xcopy /s /f /y Haketon\ThirdParty\premake5\GLFWpremake5.lua Haketon\ThirdParty\GLFW\premake5.lua*
+if not exist Haketon\ThirdParty\imgui\premake5.lua xcopy /s /f /y Haketon\ThirdParty\premake5\imguipremake5.lua Haketon\ThirdParty\imgui\premake5.lua*
 Haketon\ThirdParty\premake5\premake5.exe vs2022
 popd
 pause
